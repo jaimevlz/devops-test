02709333dac1dae30f8d40b4dd49108be9cf2ada
jaimevlz <jaimevlz306@gmail.com>
2021-04-08 20:53:29 -0500

    Update python App

--- sources/app.py
+++ sources/app.py
@@ -1,62 +1,14 @@
-import os
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask
 app = Flask(__name__)
 
-app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+@app.route('/')
+@app.route('/hello/')
+def hello_world():
+    return 'Hello World!\n'
 
-from models import Client
-
-#Index
-@app.route("/")
-def index():
-    return "This is the app index"
-
-#Add client and money
-@app.route("/add")
-def add_client():
-    name=request.args.get('name')
-    money=request.args.get('money')
-    try:
-        client=Client(
-            name=name,
-            money=money
-        )
-        db.session.add(client)
-        db.session.commit()
-        return "Client added with id={}".format(client.id)
-    except Exception as e:
-	    return(str(e))
-
-#Get all clients
-@app.route("/getall")
-def get_all():
-    try:
-        clients=Client.query.all()
-        return  jsonify([e.serialize() for e in clients])
-    except Exception as e:
-	    return(str(e))
-
-#Get client by ID
-@app.route("/get/<id_>")
-def get_by_id(id_):
-    try:
-        client=Client.query.filter_by(id=id_).first()
-        return jsonify(client.serialize())
-    except Exception as e:
-	    return(str(e))
-
-#Get client by Name
-@app.route("/getn/<name_>")
-def get_by_name(name_):
-    try:
-        client=Client.query.filter_by(name=name_).first()
-        return jsonify(client.serialize())
-    except Exception as e:
-	    return(str(e))
+@app.route('/hello/<username>') # dynamic route
+def hello_user(username):
+    return 'Why Hello %s!\n' % username
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')     # open for everyone
