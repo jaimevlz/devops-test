08f319a6e51c72f8800b9dedb94edf42293ccb19
jaimevlz <jaimevlz306@gmail.com>
2021-04-09 16:07:24 -0500

    Update

--- python-deploy-terraform/main.tf
+++ python-deploy-terraform/main.tf
@@ -60,27 +60,13 @@ resource "aws_instance" "pythonApp" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
+      "sudo git clone https://github.com/jaimevlz/devops-test.git",
       "sudo curl -fsSL https://get.docker.com -o get-docker.sh",
       "sudo sh get-docker.sh",
       "sudo docker --version",
-      "sudo apt-get install --fix-missing",
-      "sudo apt-get install openjdk-6-jre-headless",
-      "sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
-      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
-      "sudo apt update -qq",
-      "sudo apt-get install -y default-jre",
-      "sudo apt-get install -y deamon",
-      "sudo apt-get install -y net-tools",
-      "sudo apt-get install -y jenkins",
-      "sudo systemctl start jenkins",
-      "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
-      "sudo sh -c \"iptables-save > /etc/iptables.rules\"",
-      "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections",
-      "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
-      "sudo apt-get -y install iptables-persistent",
-      "sudo ufw allow 8080",
-      "sudo usermod -a -G docker jenkins",
-      "sudo service jenkins restart",
+      "sudo cd devops-test/python-deploy/terraform/",
+      "sudo docker build -t pythonApp",
+      "sudo docker run -d -p 5000:5000 pythonApp",    
     ]
   }
 
