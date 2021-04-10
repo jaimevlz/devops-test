87ba65c6104bb0f27274849ea9eac1165913cd89
jaimevlz <81440663+jaimevlz@users.noreply.github.com>
2021-04-10 14:44:45 -0500

    Add files via upload

--- python-deploy-terraform/app.py
+++ python-deploy-terraform/app.py
@@ -1,14 +1,51 @@
-from flask import Flask
+import os
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello/')
-def hello_world():
-    return 'Hello World!\n'
+app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/hello/<username>') # dynamic route
-def hello_user(username):
-    return 'Why Hello %s!\n' % username
+from models import Book
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/add")
+def add_book():
+    name=request.args.get('name')
+    author=request.args.get('author')
+    published=request.args.get('published')
+    try:
+        book=Book(
+            name=name,
+            author=author,
+            published=published
+        )
+        db.session.add(book)
+        db.session.commit()
+        return "Book added. book id={}".format(book.id)
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/getall")
+def get_all():
+    try:
+        books=Book.query.all()
+        return  jsonify([e.serialize() for e in books])
+    except Exception as e:
+	    return(str(e))
+
+@app.route("/get/<id_>")
+def get_by_id(id_):
+    try:
+        book=Book.query.filter_by(id=id_).first()
+        return jsonify(book.serialize())
+    except Exception as e:
+	    return(str(e))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')     # open for everyone
+    app.run()
